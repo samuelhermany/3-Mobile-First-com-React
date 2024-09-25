210acc91bb8955912d75ca21507795c05f3ed172
samuel <samuelhermany@hotmail.com>
2024-09-25 17:37:17 -0300

    exe03 feito

--- AT/src/App.jsx
+++ AT/src/App.jsx
@@ -7,14 +7,14 @@ export default function App() {
       <div>
         <a href="src/exercicio_01/index.html">TP 1</a>
         <a href="src/exercicio_02/index.html">TP 2</a>
-        <a href="src/exercicio_03/index.html">TP 3</a>''
-        <a href="src/exercicio_04/index.html">TP 4</a>
+        <a href="src/exercicio_03/index.html">TP 3</a>
+        {/* <a href="src/exercicio_04/index.html">TP 4</a>
         <a href="src/exercicio_05/index.html">TP 5</a>
         <a href="src/exercicio_06/index.html">TP 6</a>
         <a href="src/exercicio_07/index.html">TP 7</a>
         <a href="src/exercicio_08/index.html">TP 8</a>
         <a href="src/exercicio_09/index.html">TP 9</a>
-        <a href="src/exercicio_10/index.html">TP 10</a>
+        <a href="src/exercicio_10/index.html">TP 10</a> */}
         {/* <a href="src/exercicio_11/index.html">TP 11</a>
         <a href="src/exercicio_12/index.html">TP 12</a>
         <a href="src/exercicio_13/index.html">TP 13</a>
